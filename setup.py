965dd7ab7427dff61fd95eb36e47204966f39846
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2019-10-02 08:07:37 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from setuptools import setup
+from io import open
+
+def read(filename):
+    with open(filename, encoding='utf-8') as file:
+        return file.read()
+
+setup(name='KBPython',
+      version='3.6.6',
+      description='Python Telegram bot api. ',
+      long_description=read('README.md'),
+      long_description_content_type="text/markdown",
+      author='Kerala Team',
+      author_email='anandps002@gmail.com',
+      url='https://github.com/keralasbots/KBPYTHON',
+      packages=['keralasbot'],
+      license='GPL2',
+      keywords='telegram bot api tools',
+      install_requires=['requests', 'six'],
+      extras_require={
+          'json': 'ujson',
+      },
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Environment :: Console',
+          'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+      ]
+      )
