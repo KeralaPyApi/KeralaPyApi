e8589a7801195aa4102585d4fcaecfb372c1d4d2
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2019-11-04 07:47:35 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath("../.."))
 
-from KeralaPyApi import __version__
+from Keralabot import __version__
 
 from pygments.styles.friendly import FriendlyStyle
 
