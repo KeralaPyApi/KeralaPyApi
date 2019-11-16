6337b70f17b320ec5e3905e9b8ed3efdd45185e8
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2019-11-16 18:53:30 +0530

    Debugged the installation error

--- setup.py
+++ setup.py
@@ -1,46 +1,26 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
+from setuptools import setup
 from io import open
-import setuptools
-import os
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
 def read(filename):
     with open(filename, encoding='utf-8') as file:
         return file.read()
 
-def requirements():
-    """Build the requirements list for this project"""
-    requirements_list = []
-
-    with open('requirements.txt') as requirements:
-        for install in requirements:
-            requirements_list.append(install.strip())
-
-    return requirements_list
-
-
-packages = find_packages(exclude=['logos*'])
-
-
 setup(name='KeralaPyApi',
-      version='0.3.0',
-      description='Python Telegram bot api. ',
-      long_description=long_description,
+      version='1.0.2',
+      description='Telegram bot api using Python. ',
+      long_description=read('README.md'),
       long_description_content_type="text/markdown",
-      author='Kerala Team',
-      author_email='anandvfc640@gmail.com',
-      url='https://github.com/KeralaPyApi/KeralaPyApi.git',
-      packages=packages,
+      author='Mario',
+      author_email='keralapyapi@gmail.com',
+      url='https://github.com/KeralaPyApi/KeralaPyApi',
+      packages=['keralabot'],
       license='GPLv2',
-      keywords='telegram bot api tools',
-      install_requires=requirements(),
+      keywords='telegram bot api using python',
+      install_requires=['requests', 'six'],
       extras_require={
           'json': 'ujson',
       },
-      include_package_data=True,
       classifiers=[
           'Development Status :: 5 - Production/Stable',
           'Programming Language :: Python :: 2',
