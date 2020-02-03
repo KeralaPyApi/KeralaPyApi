0a0ee14c1337d1ef9a5a75dd8d8f9849d7f76c19
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-02-03 15:29:35 +0530

    Some basic corrections

--- keralabot/__init__.py
+++ keralabot/__init__.py
@@ -396,6 +396,8 @@ class bot:
 
     def __threaded_polling(self, none_stop=False, interval=0, timeout=3):
         logger.info('Started polling.')
+        logger.info("KeralaPyApi version {0} started".format(__version__))
+        logger.info("Licensed under GPLV3+ (c) <KeralaPyApi@gmail.com> KeralaPyApi.. ")
         self.__stop_polling.clear()
         error_interval = .25
 
